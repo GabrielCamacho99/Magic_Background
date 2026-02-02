c78c1d02a17e1c3f57ae53a5a0fd4f35a2703fe1
Gabriel <gabri@Gabriel_Laptop.lan>
2026-02-02 02:10:24 +0000

    JS and n8n update

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,73 @@ document.addEventListener("DOMContentLoaded", function() {
     //1st step: get submit form event to avoid page reload
     const form = document.querySelector(".form-group");
     const descriptionInput = document.getElementById("description");
+    const htmlCode = document.getElementById("html-code");
+    const cssCode = document.getElementById("css-code");
+    const previewSection = document.getElementById("preview-section"); 
     
-    form.addEventListener("submit", function(event) {
+    form.addEventListener("submit", async function(event) {
         event.preventDefault(); // avoid reload
 
         //2nd step: obtain user textarea input
-        const description = descriptionInput.value;
-        console.log(description);
-    })
+        const description = descriptionInput.value.trim();
+        
+        if(!description) {
+            return;
+        }
+        //3rd step Show a loading indicator while requisition is being processed
+        showLoading(true);
+
+        //POST request to n8n API
+        try{
+            const answer = await fetch("https://gabriel31299.app.n8n.cloud/webhook-test/magic-background",{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({description})
+            });
+
+            const data = await answer.json();
+            htmlCode.textContent = data.html || "";
+            cssCode.textContent = data.css || "";
+
+            previewSection.style.display = "block";
+            previewSection.innerHTML = data.html || "";
+
+            //Apply CSS to our preview-section
+            let tagStyle = document.getElementById("dynamic-style");
+            // if this tag already exists , remove before creating new
+            if(tagStyle) {
+                tagStyle.remove();
+            }
+
+            if(data.css) {
+                tagStyle = document.createElement("style");
+                tagStyle.id = "dynamic-style";
+                tagStyle.textContent = data.css;
+                tagStyle.head.appendChild(tagStyle);
+            }
+            
+
+        }catch(error){
+            console.error("Error while sending request: ", error);
+            htmlCode.textContent = "Couldn't generate HTML , try again please.";
+            cssCode.textContent = "Couldn't generate CSS , try again please.";
+            previewSection.innerHTML = "";
+        }finally{
+            showLoading(false);
+        }
+    });
+
+    function showLoading (isLoading) {
+        const buttonSend = document.getElementById("gen-btn");
+        if(isLoading) {
+            buttonSend.textContent = "Generating your Magic Background..."
+        }else {
+            buttonSend.textContent = "Generate your Magic Background..."
+        }
+    }
+
 });
 
 
